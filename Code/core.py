6f4f4fb6c06c2324688ff81cd5f8e3d09c41d094
shrobon <shrobon.biswas@gmail.com>
2017-02-22 22:17:03 -0700

    Added Functions for contours and Threshold

--- Code/core.py
+++ Code/core.py
@@ -1,6 +1,7 @@
 import cv2
-from projectutils import play_video
+from projectutils import play_video,count_cars
 
 video_file = '../Dataset/StAlbert/traffic_south.MP4'
 window_name= "Target Video" 
-play_video(video_file,25,window_name)
+#play_video(video_file,25,window_name)
+count_cars(video_file,25,window_name)
--- Code/projectutils.py
+++ Code/projectutils.py
@@ -19,3 +19,33 @@ def play_video(video_file,sleepTime,display_window_name):
 
 	cap.release()
 	cv2.destroyAllWindows()
+
+
+#This function will count the total number of cars in the video
+def count_cars(video_file,sleepTime,display_window_name):
+	
+	cv2.namedWindow(display_window_name)
+	cap = cv2.VideoCapture(video_file)
+	while(cap.isOpened()):
+		#reading frame by frame
+		ret, frame = cap.read()
+
+		#trying otsu thresholding
+		gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+		ret1, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+		
+		#Detecting contours
+		(_,cnt,_) = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+		cv2.drawContours(frame,cnt,-1,(0,255,0),3)
+		cv2.imshow(display_window_name,frame)
+
+
+
+
+		#Video stops playing if 'q' or escapse is pressed
+		#Change the frame rate according to application
+		if cv2.waitKey(25) == ord('q'):
+			break
+
+	cap.release()
+	cv2.destroyAllWindows()
