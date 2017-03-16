f32f91544fa8300340c4cd2ef1115c0ad6f9cb2c
shrobon <shrobon.biswas@gmail.com>
2017-03-16 00:49:18 -0600

    wrong counting :: Need Better Logic

--- Code/projectutils.py
+++ Code/projectutils.py
@@ -23,7 +23,7 @@ def play_video(video_file,sleepTime,x1,y1,x2,y2,trim_begin,trim_end,display_wind
 
 #This function will count the total number of cars in the video
 def count_cars(video_file,sleepTime,x1,y1,x2,y2,trim_begin,trim_end,display_window_name):
-	
+	target_row =  600
 	cv2.namedWindow(display_window_name)
 	cap = cv2.VideoCapture(video_file)
 	reference_frame = None
@@ -42,8 +42,8 @@ def count_cars(video_file,sleepTime,x1,y1,x2,y2,trim_begin,trim_end,display_wind
 			exit()
 
 		frame = frame[:,500:1100,:]
-		frame = cv2.line(frame,(100,650),(590,600),(255,0,255),10)
-		
+		#frame = cv2.line(frame,(100,650),(600,600),(255,0,255),5)
+		frame = cv2.line(frame,(100,600),(600,600),(0,0,255),5)
 		
 		#Getting the reference frame
 		if ret is not None and reference_frame is None:
@@ -84,15 +84,28 @@ def count_cars(video_file,sleepTime,x1,y1,x2,y2,trim_begin,trim_end,display_wind
 			#need to find a suitable area range to filter the contours
 			contour_area = cv2.contourArea(contour)
 			if (contour_area >0.009 * image_area) and  (contour_area <0.10* image_area):
-
+				
+				### Moments are required to calculate the center point ###
+				M = cv2.moments(contour)
+				cX = int(M["m10"] / M["m00"])
+				cY = int(M["m01"] / M["m00"])
+				cv2.circle(frame, (cX, cY), 1, (255,111,251), 5)
+				### The center point will be used to count the cars #######
 				(x,y,w,h) = cv2.boundingRect(contour)
-				cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+				#cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+
 
 
 				#cv2.drawContours(frame,cnt,-1,(0,255,0),3)
 				cv2.imshow(display_window_name,frame)
 
-
+				#### Include the logic of line crossing ###########	
+				#### Logic 1 : Count the number of Color dots crossing the line
+				line_check_array = frame[600,100:600,:]
+				cv2.imshow("line",line_check_array)
+				for x in line_check_array:
+					if x[0] == 255 :
+						print True
 
 
 		#Video stops playing if 'q' or escapse is pressed
